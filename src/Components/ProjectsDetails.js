f4b0a4a09cda59ab1f200821625b7486cc32338c
Nizami Suleymanov <nizami.suleymanoff@gmail.com>
2023-01-19 19:02:39 +0100

    added Projects Details

---
+++ src/Components/ProjectsDetails.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Box, Flex, Heading } from '@chakra-ui/react';
+import ProjectCard from './ProjectCard';
+
+const ProjectsOverview = () => {
+  return (
+    <>
+      <Flex
+        maxW="960px"
+        mx="auto"
+        id="projectsOverview"
+        direction="row"
+        align="center"
+        justify="center"
+      >
+        <Flex
+          w="100%"
+          mx={6}
+          py={16}
+          borderTop="1px"
+          borderColor="orange.300"
+          direction="column"
+        >
+          <Heading as="h2" size="2xl">
+            My Projects
+          </Heading>
+          <Box mt={16}>
+            <ProjectCard />
+          </Box>
+        </Flex>
+      </Flex>
+    </>
+  );
+};
+
+export default ProjectsOverview;
