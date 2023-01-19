c9f07a8c48097ae8b6483a905a3a20adf9edb497
Nizami Suleymanov <nizami.suleymanoff@gmail.com>
2023-01-19 19:00:41 +0100

    added project

---
+++ src/Components/Contact.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import {
+  Heading,
+  Text,
+  IconButton,
+  ButtonGroup,
+  Link,
+  Flex,
+  Box,
+  Button,
+  Image,
+  Show,
+} from '@chakra-ui/react';
+import Obfuscate from 'react-obfuscate';
+
+// Icons
+import { EmailIcon } from '@chakra-ui/icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faLinkedinIn,
+  faGithub,
+  faDiscord,
+} from '@fortawesome/free-brands-svg-icons';
+
+const Contact = () => {
+  const contactOptions = [
+    {
+      option: 'LinkedIn',
+      reactIcon: faLinkedinIn,
+      href: 'https://www.linkedin.com/in/lydiarobnik/',
+      aria: 'view LinkedIn profile',
+    },
+    {
+      option: 'GitHub',
+      reactIcon: faGithub,
+      href: 'https://www.github.com/LydiaRobnik',
+      aria: 'view GitHub profile',
+    },
+    {
+      option: 'Discord',
+      reactIcon: faDiscord,
+      href: 'https://discordapp.com/users/982315750628950017',
+      aria: 'view Discord profile',
+    },
+  ];
+
+  return (
+    <>
+      <Flex
+        maxW="960px"
+        mx="auto"
+        id="contact"
+        direction="row"
+        align="center"
+        justify="center"
+      >
+        <Flex
+          w="100%"
+          mx={6}
+          pt={16}
+          direction="column"
+          borderTop="1px"
+          borderColor="orange.300"
+        >
+          <Heading as="h2" size="2xl">
+            Get In Touch
+          </Heading>
+          <Flex direction="row">
+            <Box>
+              <Text py={1} mt={6} maxW="600px">
+                Dropping a line to say "hello", get connected on social media or
+                see if we can build something amazing together? I’d love to hear
+                from you!
+              </Text>
+
+              <Flex justify="start">
+                <ButtonGroup spacing="16px" my={12}>
+                  <Obfuscate
+                    email="LydiaRobnik@women-at-work.org"
+                    headers={{
+                      subject: 'Nice portfolio',
+                      body: 'Hi Lydia, ...',
+                    }}
+                  >
+                    <IconButton
+                      variant="outline"
+                      colorScheme="cyan"
+                      aria-label="send email"
+                      icon={<EmailIcon />}
+                      p={2}
+                      cursor="pointer"
+                    />
+                  </Obfuscate>
+                  {contactOptions.map(option => (
+                    <Link key={option.option} href={option.href} isExternal>
+                      <Button
+                        variant="outline"
+                        aria-label={option.aria}
+                        p={2}
+                        cursor="pointer"
+                        colorScheme="cyan"
+                      >
+                        <FontAwesomeIcon icon={option.reactIcon} size="lg" />
+                      </Button>
+                    </Link>
+                  ))}
+                </ButtonGroup>
+              </Flex>
+            </Box>
+            <Show above="md">
+              <Image
+                src="https://storage.googleapis.com/portfolioly-bucket/bird_250px.png"
+                alt="illustration of a bird"
+                align="center"
+                maxW="250px"
+              />
+            </Show>
+          </Flex>
+        </Flex>
+      </Flex>
+    </>
+  );
+};
+
+export default Contact;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,133 @@
+import React, { useState } from 'react';
+import { Link } from 'react-scroll';
+import { Flex, Button, IconButton, useColorModeValue } from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { ColorModeSwitcher } from '../ColorModeSwitcher';
+import '../Styles/navbar.css';
+
+const Navbar = () => {
+  const bg = useColorModeValue('white', 'gray.800');
+  const [isOpen, setIsOpen] = useState('none');
+
+  const navigation = [
+    { name: 'Home', path: 'home' },
+    { name: 'About', path: 'about' },
+    { name: 'Skills', path: 'skills' },
+    { name: 'Projects', path: 'projectsOverview' },
+    { name: 'Contact', path: 'contact' },
+  ];
+
+  const handleClick = () => {
+    setIsOpen(isOpen === 'none' ? 'block' : 'none');
+  };
+
+  return (
+    <>
+      <nav>
+        {/* navbar screen size > lg */}
+        <Flex
+          w="100%"
+          pos="fixed"
+          top="-1px"
+          right="0"
+          align="center"
+          justify="end"
+          bg={bg}
+          boxShadow="sm"
+          py={3}
+          px={3}
+          zIndex={50}
+        >
+          <Flex display={['none', 'none', 'flex', 'flex']}>
+            {navigation.map(nav => (
+              <Link
+                smooth={true}
+                duration={1200}
+                offset={-20}
+                spy={true}
+                hashSpy={true}
+                activeClass="active"
+                key={nav.name}
+                to={nav.path}
+              >
+                <Button
+                  variant="ghost"
+                  aria-label={nav.name}
+                  aria-current={nav.current ? `${nav.name}` : undefined}
+                  my={1}
+                  w="100%"
+                >
+                  {nav.name}
+                </Button>
+              </Link>
+            ))}
+          </Flex>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <IconButton
+            aria-label="Open Menu"
+            size="lg"
+            mx={2}
+            icon={<HamburgerIcon />}
+            display={['flex', 'flex', 'none', 'none']}
+            onClick={handleClick}
+          />
+        </Flex>
+
+        {/* mobile menue, screen size < lg */}
+        <Flex
+          w="100vw"
+          bg={bg}
+          zIndex={50}
+          h="100vh"
+          pos="fixed"
+          top="0"
+          left="0"
+          overflow="auto"
+          direction="column"
+          display={isOpen}
+        >
+          <Flex justify="flex-end">
+            <IconButton
+              aria-label="Close Menu"
+              size="lg"
+              mt={2}
+              mr={2}
+              icon={<CloseIcon />}
+              onClick={handleClick}
+            />
+          </Flex>
+
+          <Flex direction="column" align="center">
+            {navigation.map(nav => (
+              <Link
+                smooth={true}
+                duration={1200}
+                offset={-100}
+                spy={true}
+                hashSpy={true}
+                activeClass="active"
+                key={nav.name}
+                to={nav.path}
+              >
+                <Button
+                  variant="ghost"
+                  aria-label={nav.name}
+                  aria-current={nav.current ? `${nav.name}` : undefined}
+                  my={5}
+                  w="100%"
+                  onClick={() => {
+                    handleClick();
+                  }}
+                >
+                  {nav.name}
+                </Button>
+              </Link>
+            ))}
+          </Flex>
+        </Flex>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
